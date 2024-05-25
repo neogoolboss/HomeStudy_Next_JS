8b90b7389d3d35a56b0852996a5ac22713787304
neogoolboss <krossjsnet88@gmail.com>
2024-05-25 23:29:57 +0900

    NEXT JS Map

--- app/list/page.js
+++ app/list/page.js
@@ -1,15 +1,25 @@
 export default function List() {
 
+    let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
+    // let Array = [2,3,4]
+    // let test = Array.map((a) => {
+    //             return 10
+    //         })
+    //         console.log(test);
+
     return (
 
         <div>
             <h4 className="title">상품 목록</h4>
-            <div className="food">
-                <h4>상품1 $40</h4>
-            </div>
-            <div className="food">
-                <h4>상품2 $40</h4>
-            </div>
+            {
+                상품.map((a, i) => {
+                    return (
+                        <div className="food">
+                            <h4>{상품[i]} $40</h4>
+                        </div>
+                    )
+                })
+            }
         </div>
 
     )
