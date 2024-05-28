9385b5f8815be32b370ad4911a22e7f46c36a190
neogoolboss <krossjsnet88@gmail.com>
2024-05-28 22:06:26 +0900

    2024 05 28

---
+++ app/cart/data.js
@@ -0,0 +1,6 @@
+let age = 20;
+let name = 'kim';
+
+// export default age;
+
+export {age, name};
--- app/cart/page.js
+++ app/cart/page.js
@@ -1,20 +1,26 @@
+import {age, name} from './data.js'
+
 export default function Cart() {
 
     return (
 
         <div>
             <h4 className="title">Cart</h4>
-            <div className="cart-item">
-                <p>상품명</p>
-                <p>$40</p>
-                <p>1개</p>
-            </div>
-            <div className="cart-item">
-                <p>상품명</p>
-                <p>$40</p>
-                <p>1개</p>
-            </div>
+            <CartItem/>
+            <CartItem/>
+            <CartItem/>
         </div>
 
+    )
+}
+
+// 컴포넌트 형식
+function CartItem() {
+    return (
+        <div className="cart-item">
+            <p>상품명</p>
+            <p>$40</p>
+            <p>1개</p>
+        </div>
     )
 }
