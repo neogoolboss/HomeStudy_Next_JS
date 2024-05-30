0f94beef7e8f10843779e3a711414f1aaa7e67ca
neogoolboss <krossjsnet88@gmail.com>
2024-05-30 22:05:01 +0900

    2024 05 30 props

--- app/cart/page.js
+++ app/cart/page.js
@@ -2,23 +2,31 @@ import {age, name} from './data.js'
 
 export default function Cart() {
 
+    let 장바구니 = ['Tomatoes', 'Pasta']
+    let 카드 = ['현대', '신한']
+
     return (
 
         <div>
             <h4 className="title">Cart</h4>
-            <CartItem/>
-            <CartItem/>
-            <CartItem/>
+            <CartItem 상품={장바구니[0]}/>
+            <CartItem 상품={장바구니[1]}/>
+            <Banner 카드 = {카드[0]}/>
+            <Banner 카드 = {카드[1]}/>
         </div>
 
     )
 }
 
+function Banner(props) {
+    return <h5>{props.카드}카드 결제 행사중</h5>
+}
+
 // 컴포넌트 형식
-function CartItem() {
+function CartItem(props) {
     return (
         <div className="cart-item">
-            <p>상품명</p>
+            <p>{props.상품}</p>
             <p>$40</p>
             <p>1개</p>
         </div>
