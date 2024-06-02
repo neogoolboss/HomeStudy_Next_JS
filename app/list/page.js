9eb8dea0c9e97e2abca3e80d7d88d13a78c72b74
neogoolboss <krossjsnet88@gmail.com>
2024-06-02 23:04:58 +0900

    2024 06 02 useState

--- app/list/page.js
+++ app/list/page.js
@@ -1,4 +1,7 @@
+'use client'
+
 import Image from "next/image"
+import {useState} from "react"
 
 export default function List() {
 
@@ -8,6 +11,8 @@ export default function List() {
     //             return 10
     //         })
     //         console.log(test);
+    // let 수량 = 0;
+    let [수량, 수량변경] = useState(0)
 
     return (
 
@@ -19,6 +24,13 @@ export default function List() {
                         <div className="food" key={i}>
                             <img src={`/food${i}.png`} className="food-img"/>
                             <h4>{상품[i]} $40</h4>
+                            <span > {수량} </span>
+                            {/* <button onMouseOver={() => {
+                                console.log('눌림');
+                            }}> + </button> */}
+                            <button onClick={() => {
+                                수량변경(수량+1);
+                            }}> + </button>
                         </div>
                     )
                 })
